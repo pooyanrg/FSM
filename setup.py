c7fff9f7e2e134ef3a0217c66a7bed96171565d0
Pooyan <xintium@Pooyans-MacBook-Pro.local>
2024-02-15 15:12:31 -0600

    added setup file

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='fsm',
-     py_modules=["fsm"],
+    py_modules=["fsm"],
     version='1.0',
     description='',
     packages=find_packages(),
