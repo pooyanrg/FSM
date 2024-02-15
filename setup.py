c972191e961dc83fe28c2df43a36a2a6bedd7aef
Pooyan <xintium@Pooyans-MacBook-Pro.local>
2024-02-15 14:52:23 -0600

    added setup file

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='fsm',
+    version='1.0',
+    description='',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'fado',
+        'graphviz',
+        'pandas',
+        'json',
+    ],
+)
